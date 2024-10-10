2095f7dae64fc195a0121c7124f83691d4099fca
zhuzhenquan <2332778528@qq.com>
2024-10-10 14:40:20 +0800

    feat: empty chat

---
+++ src/pages/chat/EmptyChat.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts" name="EmptyChat">
+import { Button, Layout } from "ant-design-vue";
+import { t } from "i18next";
+
+import empty_chat_bg from "@/assets/images/empty_chat_bg.png";
+
+const createNow = () => {
+  // emitter.emit("OPEN_CHOOSE_MODAL", {
+  //   type: "CRATE_GROUP",
+  // });
+
+  console.log("createNow");
+};
+</script>
+
+<template>
+  <Layout class="no-mobile flex items-center justify-center bg-white">
+    <div>
+      <div class="mb-12 flex flex-col items-center">
+        <div class="mb-3 text-xl font-medium">
+          {{ t("placeholder.createGroup") }}
+        </div>
+        <div class="text-[var(--sub-text)]">
+          {{ t("placeholder.createGroupToast") }}
+        </div>
+      </div>
+      <img :src="empty_chat_bg" alt="" width="320" />
+
+      <div class="mt-28 flex justify-center">
+        <Button class="px-8" type="primary" @click="createNow">
+          {{ t("placeholder.createNow") }}
+        </Button>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<style scoped lang="scss"></style>
--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts" name="index">
 import { Layout } from "ant-design-vue";
 import {} from "vue";
+import { RouterView } from "vue-router";
+import ConversationSider from "./ConversationSider/index.vue";
 </script>
 
 <template>
-  <Layout class="flex-row"> chat </Layout>
+  <Layout class="flex-row">
+    <ConversationSider />
+    <router-view />
+  </Layout>
 </template>
 
 <style scoped lang="scss"></style>
