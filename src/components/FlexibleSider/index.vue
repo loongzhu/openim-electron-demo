b59531eb21d088b46ed644c75191541a6369d2d5
zhuzhenquan <2332778528@qq.com>
2024-10-10 13:18:54 +0800

    feat: flexible silder component

---
+++ src/components/FlexibleSider/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts" name="index">
+import { toRefs } from "vue";
+
+interface Props {
+  needHidden?: boolean;
+  wrapClassName: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  needHidden: false,
+  wrapClassName: "",
+});
+
+const { needHidden, wrapClassName } = toRefs(props);
+</script>
+
+<template>
+  <aside
+    class="relative bg-white dark:text-white"
+    :class="{
+      'max-[600px]:hidden': needHidden,
+      'max-[600px]:!max-w-none max-[600px]:!basis-full': !needHidden,
+    }"
+  >
+    <div
+      class="absolute bottom-0 left-0 right-1 top-0 z-10 overflow-hidden"
+      :class="wrapClassName"
+    >
+      <slot></slot>
+    </div>
+    <div class="sider_resize"></div>
+    <div class="sider_bar"></div>
+  </aside>
+</template>
+
+<style scoped lang="scss">
+@use "./flexible-sider.module.scss" as styles;
+</style>
